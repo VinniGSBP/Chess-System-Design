af3be0c7f807d674c79dcc3d5fb20b6d8e5ffa9d
VinniGSBP <gs.vinni@gmail.com>
2025-11-29 00:02:30 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessExecption.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessExecption extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	
+	public ChessExecption(String msg) {
+		super(msg);
+	}
+}
